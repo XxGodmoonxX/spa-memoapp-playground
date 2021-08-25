411f9b7644bfef2b2c890d82a3f45c693c7473fe
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2021-08-25 20:07:45 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
+export default function Home(): JSX.Element {
+  return <Title>My page</Title>
+}
+
 const Title = styled.h1`
   color: red;
   font-size: 50px;
 `
-
-export default function Home() {
-  return <Title>My page</Title>
-}
